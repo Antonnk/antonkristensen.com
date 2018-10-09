aa84990f183e8d20bd27538ed928f37c22753717
Anton Kristensen <antonnk96@gmail.com>
2018-10-10 00:49:32 +0200

    Added Fonts

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -4,7 +4,6 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-		<link href="https://fonts.googleapis.com/css?family=Lora:400,700|Stoke" rel="stylesheet">
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
     </head>
     <body class="font-serif bg-blue text-white noice-bg">
